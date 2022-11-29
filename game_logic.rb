0a01fb8bffa352e47d6e17d3dd7f9d45cad80659
Pascal <pascalyuen@gmail.com>
2022-11-29 20:26:12 +0800

    Remove byebug

--- game_logic.rb
+++ game_logic.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'display'
-require 'pry-byebug'
 
 # game logic for mastermind
 module GameLogic
--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'bundler'
+Bundler.require(:default)
 require_relative 'game'
 
 game = Game.new
